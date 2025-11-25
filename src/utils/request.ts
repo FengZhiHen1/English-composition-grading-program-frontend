02fc05f3bdbf155c056625d590cb889e3d618f40
FengZhiHen1 <lwr0801430@163.com>
2025-11-25 18:38:49 +0800

    修复了无法上传文件的bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // 后端基础地址
   timeout: 10000, // 请求超时时间
   headers: {
-    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
